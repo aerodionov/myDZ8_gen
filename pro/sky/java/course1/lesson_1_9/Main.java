4a65f5ff2bfd42f2b5a369f6d683a56be9c8cf06
aerod <aerodionov@mail.ru>
2022-07-06 23:19:01 +0300

    Первый коммит

---
+++ pro/sky/java/course1/lesson_1_9/Main.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course1.lesson_1_9;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+    }
+}
